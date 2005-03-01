dbeb1f192e1f2a5dc70b217909df6e1a16bc81b0
Jeff Davis <davis@xarg.net>
2005-03-01 00:01:41 +0000

    change ad_verify_and_get_user_id to ad_conn user_id, change ad_maybe_redirect_for_registration to auth::require_login, make some db_ and dt_ function -public

--- www/zip-check.tcl
+++ www/zip-check.tcl
@@ -13,7 +13,7 @@ set package_id [ad_conn package_id]
 set package_url [ad_conn package_url]
 set package_admin_p [ad_permission_p $package_id admin]
 ad_require_permission $package_id admin
-set this_user_id [ad_verify_and_get_user_id]
+set this_user_id [ad_conn user_id]
 
 set system_name [ad_parameter -package_id [ad_acs_kernel_id] SystemName]
 
